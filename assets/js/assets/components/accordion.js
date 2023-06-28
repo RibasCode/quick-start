bb2b9886833b90c4602093edce967332a5501fa4
Marc Ribas <ribascode@gmail.com>
2023-06-28 19:42:35 +0200

    feature: dark theme

--- assets/js/assets/components/accordion.js
+++ assets/js/assets/components/accordion.js
@@ -1,13 +1,9 @@
+
 $(document).ready(function() {
 
-	let accordionButton = $('nav .accordion-button');
-
-	accordionButton.click(function(){
-		let $this = $(this);
-
-		$this.toggleClass('active');
-		$(`.accordion-body`).toggleClass('active').slideToggle(250);
-
-        checkFilterIfInnerItemsActive();
+	$('.accordion').click(function(){
+		$(this).toggleClass('active');
+        $(this).find('.content').slideToggle(250);
 	});
+
 });
