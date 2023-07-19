d3893a5123a96f9903be31d9e7bcde1e5450cdb4
Marc Ribas <ribascode@gmail.com>
2023-07-19 21:36:55 +0200

    Improvement: remove toast library

--- assets/js/assets/app.js
+++ assets/js/assets/app.js
@@ -1,13 +1,8 @@
 
 'use strict'
 
-import Toast from "./components/Toast.js"
+$(document).ready(function() {
 
-document.querySelector("button.toast-button").addEventListener("click", () => {
-  const toast = new Toast({
-    text: "Hello",
-    position: "bottom-right",
-    pauseOnHover: true,
-    pauseOnFocusLoss: true,
-  })
-})
+
+
+});
