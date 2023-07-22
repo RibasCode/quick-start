7458e200bde4169c0eb9ba88d85990c3a6b98eec
Marc Ribas <ribascode@gmail.com>
2023-07-22 11:58:30 +0200

    Improvement: implement toastify-js as a project dependency

--- assets/js/assets/app.js
+++ assets/js/assets/app.js
@@ -3,6 +3,19 @@
 
 $(document).ready(function() {
 
-
+    $('.toast-button').click(() => {
+        Toastify({
+            text: "This is a toast",
+            className: "info",
+            duration: 3000,
+            destination: "https://github.com/apvarun/toastify-js",
+            newWindow: true,
+            close: true,
+            gravity: "bottom", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            onClick: function(){} // Callback after click
+        }).showToast();
+    });
 
 });
