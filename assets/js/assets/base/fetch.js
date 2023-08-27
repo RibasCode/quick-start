cd96b13cfde7e49fec48317b4063f4900a9a5069
Marc Ribas <ribascode@gmail.com>
2023-08-27 20:29:04 +0200

    Revert "quick-start v2.0"
    This reverts commit bf2b0583e11000e4e2a6ae216bd443773f1ccf3b.

---
+++ assets/js/assets/base/fetch.js
@@ -0,0 +1,24 @@
+
+'use strict'
+
+$(document).ready(function() {
+
+    const fetchName = () => {
+
+        fetch("assets/documents/cards.json")
+        .then(response => response.json())
+        .then(data => {
+
+            console.log('Success:', data);
+
+            // for(let i = 0; i < data.length; i++){
+
+            // };
+
+        }).catch( error => console.log('Error:', error));
+
+    };
+
+    fetchName();
+
+});
