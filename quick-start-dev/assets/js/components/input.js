bc623f297eb729ffe6353eacf9c78f295a7c286b
Marc Ribas <ribascode@gmail.com>
2024-05-12 10:53:02 +0200

    HTML, CSS, JS: input, textarea

---
+++ quick-start-dev/assets/js/components/input.js
@@ -0,0 +1,50 @@
+
+"use strict"
+
+$(document).ready(function() {
+
+    let input = $('input');
+
+    input.each( function(){
+        let $this = $(this);
+
+        if($this.val() != ''){
+            $this.closest('.input-container').addClass('status-focus');
+        }
+    });
+
+    input.focusin(function() {
+        $(this).closest('.input-container').addClass('status-focus');
+    });
+
+    input.focusout(function() {
+        let $this = $(this);
+
+        if($this.val() == ''){
+            $this.closest('.input-container').removeClass('status-focus');
+        }
+    });
+
+    let textarea = $('textarea');
+
+    textarea.each( function(){
+        let $this = $(this);
+
+        if($this.val() != ''){
+            $this.closest('.textarea-container').addClass('status-focus');
+        }
+    });
+
+    textarea.focusin(function() {
+        $(this).closest('.textarea-container').addClass('status-focus');
+    });
+
+    textarea.focusout(function() {
+        let $this = $(this);
+
+        if($this.val() == ''){
+            $this.closest('.textarea-container').removeClass('status-focus');
+        }
+    });
+
+});
