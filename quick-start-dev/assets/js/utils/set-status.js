72c04bece61fba15b42faa7bc64658a0f143c383
Marc Ribas <ribascode@gmail.com>
2024-11-07 22:50:49 +0100

    set-status.js

--- quick-start-dev/assets/js/utils/set-status.js
+++ quick-start-dev/assets/js/utils/set-status.js
@@ -83,15 +83,3 @@ export function setStatus (object) {
     }
   }
 };
-
-export function removeElements (object) {
-  let { elements = null } = object
-
-  if (!elements) throw new Error("Must specify some elements to remove")
-
-  for (let i = 0; i < elements.length; i++) {
-    let element = $(elements[i])
-
-    element.remove()
-  }
-}
