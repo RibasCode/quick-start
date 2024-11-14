e0051d8f1e02ae644f9865ecdfa71e6e0840de0b
Marc Ribas <ribascode@gmail.com>
2024-11-14 22:00:57 +0100

    import function into type="module" script

--- quick-start/assets/js/template.js
+++ quick-start/assets/js/template.js
@@ -1,5 +1,7 @@
 "use strict"
 
+// import { setStatus } from "../utils/set-status.js" you can import an utils function into a type="module" script without having to declare in the HTML file
+
 // $(document).ready is not necesary if <script src="url.js" defer></script>
 
 // JavaScript
