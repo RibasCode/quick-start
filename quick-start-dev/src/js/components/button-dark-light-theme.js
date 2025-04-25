b51932e296809e46c643f8b425c3aca26475f2e4
Marc Ribas <ribascode@gmail.com>
2025-04-25 22:46:21 +0200

    remove prefersColorScheme function and related imports; add button-dark-light-theme styles

--- quick-start-dev/src/js/components/button-dark-light-theme.js
+++ quick-start-dev/src/js/components/button-dark-light-theme.js
@@ -9,12 +9,3 @@ containerButtonTheme.click(function () {
   $this.find(".icon-theme").toggleClass("status-active")
   body.toggleClass("light-theme dark-theme")
 })
-
-export function prefersColorScheme () {
-  const isLightTheme = window.matchMedia && window.matchMedia("(prefers-color-scheme: light)").matches
-
-  if (isLightTheme) {
-    console.log("Light theme")
-    containerButtonTheme.click()
-  }
-}
--- quick-start/src/js/index.js
+++ quick-start/src/js/index.js
@@ -1,16 +1,12 @@
 "use strict"
 
 import { initFadeOutWebAnimation } from "./base/animation.js"
-import { prefersColorScheme } from "./components/button-dark-light-theme.js"
 // import { fetchName } from "./base/fetch.js"
 
 // init fade in web animation
 initFadeOutWebAnimation({
   startAnimation: 640
 })
-
-// Detect if the user prefers a light theme using the prefers-color-scheme media query (defaulds to dark and apply the light theme if so)
-prefersColorScheme()
 
 $(document).ready(function () {
   // fetchName()
